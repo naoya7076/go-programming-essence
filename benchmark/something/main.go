ebd35ad39c1a1c4603edc9a4431bf6331e19181c
naoya7076 <naoya7076@gmail.com>
2024-05-07 08:32:20 +0900

    メモリ改善したプログラム

--- benchmark/something/main.go
+++ benchmark/something/main.go
@@ -3,9 +3,10 @@ package something
 import "fmt"
 
 func makeSomething(n int) []string {
-	var r []string
+	// NOTE: -countで6回以上実行する必要がある。
+	r := make([]string, n)
 	for i := 0; i < n; i++ {
-		r = append(r, fmt.Sprintf("%05d 何か", i))
+		r[i] = fmt.Sprintf("%05d 何か", i)
 	}
 	return r
 }
