54399a4cb42ebd071dca434bdc401fe5a8e5d27e
naoya7076 <naoya7076@gmail.com>
2024-05-06 19:50:42 +0900

    スロットリングの追加

--- throttling/main.go
+++ throttling/main.go
@@ -12,13 +12,16 @@ func downloadJSON(u string) {
 }
 func main() {
 	before := time.Now()
+	limit := make(chan struct{}, 20)
 	var wg sync.WaitGroup
 	for i := 1; i <= 100; i++ {
 		wg.Add(1)
 		go func(i int) {
+			limit <- struct{}{}
 			defer wg.Done()
 			u := fmt.Sprintf("http://example.com/api/users?id=%d", i)
 			downloadJSON(u)
+			<-limit
 		}(i)
 	}
 	wg.Wait()
