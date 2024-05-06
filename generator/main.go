5d30603a4824eda74af196d29bb1eeb833bd8de8
naoya7076 <naoya7076@gmail.com>
2024-05-06 11:10:21 +0900

    generatorの素振り

---
+++ generator/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func generator(msg string) <-chan string {
+	ch := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			ch <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Second)
+		}
+	}()
+	return ch
+}
+
+func main() {
+	ch := generator("hello")
+	for i := 0; i < 5; i++ {
+		fmt.Println(<-ch)
+	}
+}
