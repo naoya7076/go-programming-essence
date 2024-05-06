fe115ba0f2c61482025f564d2885159b8c616e66
naoya7076 <naoya7076@gmail.com>
2024-05-06 20:27:39 +0900

    parallelを使わない実装

---
+++ calc/main_test.go
@@ -0,0 +1,25 @@
+package calc_test
+
+import (
+	"my-app/calc"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	tests := []struct {
+		name string
+		lhs  int
+		rhs  int
+		want int
+	}{
+		{name: "test1", lhs: 0, rhs: 1, want: 1},
+		{name: "test2", lhs: 1, rhs: -1, want: 0},
+		{name: "test3", lhs: 2, rhs: 1, want: 3},
+	}
+	for _, test := range tests {
+		got := calc.Add(test.lhs, test.rhs)
+		if got != test.want {
+			t.Errorf("%v: want %v, but %v", test.name, test.want, got)
+		}
+	}
+}
