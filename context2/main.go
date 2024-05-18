350372d8a342648ddb83a96cf2eca785824e49fd
naoya7076 <naoya7076@gmail.com>
2024-05-18 12:15:19 +0900

    並列のgoroutineを考える

--- context2/main.go
+++ context2/main.go
@@ -8,25 +8,17 @@ import (
 
 func main() {
 	ctx0 := context.Background()
-	ctx1, _ := context.WithCancel(ctx0)
-	//G1
+	ctx1, cancel1 := context.WithCancel(ctx0)
 	go func(ctx1 context.Context) {
-		ctx2, cancel2 := context.WithCancel(ctx1)
-		//G2-1
-		go func(ctx2 context.Context) {
-			// G2-2
-			go func(ctx2 context.Context) {
-				<-ctx2.Done()
-				fmt.Println("G2-2 canceled")
-			}(ctx2)
-
-			<-ctx2.Done()
-			fmt.Println("G2-1 canceled")
-		}(ctx2)
-		cancel2()
-
 		<-ctx1.Done()
-		fmt.Println("G1 canceled")
+		fmt.Println("G1-1 canceled")
 	}(ctx1)
+
+	go func(ctx1 context.Context) {
+		<-ctx1.Done()
+		fmt.Println("G1-2 canceled")
+	}(ctx1)
+
+	cancel1()
 	time.Sleep(time.Second)
 }
