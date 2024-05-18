5edbfcf1f546e8515373fd967b18f455f55595ce
naoya7076 <naoya7076@gmail.com>
2024-05-18 11:40:07 +0900

    直列のgoroutine

--- context2/main.go
+++ context2/main.go
@@ -3,38 +3,36 @@ package main
 import (
 	"context"
 	"fmt"
-	"sync"
+	"time"
 )
 
-var wg sync.WaitGroup
-
-func generator(ctx context.Context, num int) <-chan int {
-	out := make(chan int)
-	go func() {
-		defer wg.Done() //go func() で起動したゴルーチンの処理が終了するのを待つためにここで実行
-
-	LOOP:
-		for {
-			select {
-			case <-ctx.Done():
-				break LOOP
-			case out <- num:
-			}
-		}
-		close(out)
-		fmt.Println("generator close")
-	}()
-	return out
-}
-
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	gen := generator(ctx, 1)
-	wg.Add(1)
+	ctx0 := context.Background()
+	ctx1, _ := context.WithCancel(ctx0)
+	//G1
+	go func(ctx1 context.Context) {
+		ctx2, cancel2 := context.WithCancel(ctx1)
+		//G2-1
+		go func(ctx2 context.Context) {
+			// G2-2
+			go func(ctx2 context.Context) {
+				select {
+				case <-ctx2.Done():
+					fmt.Println("G2-2 canceled")
+				}
+			}(ctx2)
 
-	for i := 0; i < 5; i++ {
-		fmt.Println(<-gen)
-	}
-	cancel()
-	wg.Wait()
+			select {
+			case <-ctx2.Done():
+				fmt.Println("G2-1 canceled")
+			}
+		}(ctx2)
+		cancel2()
+
+		select {
+		case <-ctx1.Done():
+			fmt.Println("G1 canceled")
+		}
+	}(ctx1)
+	time.Sleep(time.Second)
 }
